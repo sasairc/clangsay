0b74ec1f1f2e226588154bae552983f59311f9b3
sasairc <sasairc@ssiserver.moe.hm>
2015-06-21 02:59:36 +0900

    1.3.0

--- config.h
+++ config.h
@@ -15,7 +15,7 @@
 
 #define PROGNAME        "clangsay"
 #define VERSION         1
-#define PATCHLEVEL      2
+#define PATCHLEVEL      3
 #define SUBLEVEL        0
 #define AUTHOR          "sasairc"
 #define MAIL_TO         "sasairc@ssiserver.moe.hm"
