24a3fd844b11934da2d1492875598dfdfb5d8f00
sasairc <sasairc@ssiserver.moe.hm>
2015-08-08 01:16:52 +0900

    clangsay 1.4.4-devel (x86_64-linux-gnu)

--- src/config.h
+++ src/config.h
@@ -16,8 +16,8 @@
 #define PROGNAME        "clangsay"
 #define VERSION         1
 #define PATCHLEVEL      4
-#define SUBLEVEL        3
-#define EXTRAVERSION    ""
+#define SUBLEVEL        4
+#define EXTRAVERSION    "-devel"
 #define AUTHOR          "sasairc"
 #define MAIL_TO         "sasairc@ssiserver.moe.hm"
 
