86ed5400ba5b717e2ac679d208f51995f4653c5f
sasairc <sasairc@ssiserver.moe.hm>
2015-08-31 05:20:14 +0900

    変更が多いので、バージョンを1.5.0へ修正

--- src/config.h
+++ src/config.h
@@ -15,8 +15,8 @@
 
 #define PROGNAME        "clangsay"
 #define VERSION         1
-#define PATCHLEVEL      4
-#define SUBLEVEL        5
+#define PATCHLEVEL      5
+#define SUBLEVEL        0
 #define EXTRAVERSION    "-devel"
 #define AUTHOR          "sasairc"
 #define MAIL_TO         "sasairc@ssiserver.moe.hm"
