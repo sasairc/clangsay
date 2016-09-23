c5f98a93b75f9776878882470948d7d20a021abc
sasairc <sasairc@ssiserver.moe.hm>
2016-09-24 00:39:46 +0900

    定数WITH_REGEXの有無でregexに関する機能を選択できるよう修正

--- src/string.h
+++ src/string.h
@@ -16,22 +16,29 @@
 
 #undef  DEBUG
 #define WITH_GLIB   /* use glib */
+#define WITH_REGEX  /* use regex */
 #define LOCALE      ""
-
-#include <regex.h>
 
 extern int strrep(char* src, char* haystack, char* needle);
 extern char* strlion(int argnum, ...);
 extern int mbstrlen(char* src);
-extern int mbstrlen_with_regex(char* src, regex_t* reg);
 extern int strunesc(char* src);
 extern int strmax(int val, char** src);
-extern int strmax_with_regex(int val, char** src, regex_t* reg);
 extern int strlftonull(char* str);
 extern char** str_to_args(char* str);
 extern char* mbstrtok(char* str, char* delimiter);
 extern int trim(char* str);
 extern int strcmp_lite(const char* str1, const char* str2);
+
+#ifdef  WITH_REGEX
+#include <regex.h>
+#ifdef  WITH_GLIB
+extern int mbstrlen_with_regex(char* src, regex_t* reg);
+extern int strmax_with_regex(int val, char** src, regex_t* reg);
+/* WITH_GLIB */
+#endif
+/* WITH_REGEX */
+#endif
 
 /* YSTRING_H */
 #endif
