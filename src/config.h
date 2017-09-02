2977703550dfdf47ea660c7d283112adea3518d9
sasairc <sasairc@ssiserver.moe.hm>
2017-09-02 18:57:59 +0900

    -devel を削除

--- src/config.h
+++ src/config.h
@@ -21,7 +21,7 @@ extern "C" {
 #define VERSION         1
 #define PATCHLEVEL      7
 #define SUBLEVEL        0
-#define EXTRAVERSION    "-devel"
+#define EXTRAVERSION    ""
 #define AUTHOR          "sasairc"
 #define MAIL_TO         "sasairc@ssiserver.moe.hm"
 
