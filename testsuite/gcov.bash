3de841bd5de63cab4ea09d3722c252f4e32bcdd5
sasairc <sasairc@ssiserver.moe.hm>
2017-09-09 20:06:37 +0900

    modified:   testsuite/gcov.bash

--- testsuite/gcov.bash
+++ testsuite/gcov.bash
@@ -7,46 +7,49 @@ COWPATH="../cows/asciiart:../cows/pixelart"
 DEFAULT_COWFILE="default.cow"
 
 function test0() {
-    ${CLANGSAY} --version                               && \
-        ${CLANGSAY} --help                              && \
-        ${CLANGSAY} --list                              && \
-        ${CLANGSAY} 'arg0' 'arg1' 'arg2' 'arg4'         && \
-        echo 'stdin' | ${CLANGSAY}                      && \
-        ${CLANGSAY} -f zen-noh-milk 'filename'          && \
-        ${CLANGSAY} -f zen-noh-milk.cow 'filename.cow'  && \
-        return 0
+	${CLANGSAY} --version								&& \
+		${CLANGSAY} --help								&& \
+		${CLANGSAY} --list								&& \
+		${CLANGSAY} 'arg0' 'arg1' 'arg2' 'arg4'			&& \
+		echo 'stdin' | ${CLANGSAY}						&& \
+		${CLANGSAY} -f zen-noh-milk 'filename'			&& \
+		${CLANGSAY} -f zen-noh-milk.cow 'filename.cow'	&& \
+		return 0
 
-    return 1
+	return 1
 }
 
 
 function test1() {
-    ${CLANGSAY} -b 'borg'               && \
-        ${CLANGSAY} -d 'dead'           && \
-        ${CLANGSAY} -g 'greedy'         && \
-        ${CLANGSAY} -p 'paranoid'       && \
-        ${CLANGSAY} -s 'stoned'         && \
-        ${CLANGSAY} -t 'tired'          && \
-        ${CLANGSAY} -w 'wired'          && \
-        ${CLANGSAY} -y 'youthful'       && \
-        ${CLANGSAY} -e "目" 'eye'       && \
-        ${CLANGSAY} -T "舌" 'tongue'    && \
-        ${CLANGSAY} --think 'think'     && \
-        ${CLANGSAY} --say 'say'         && \
-        ${CLANGSAY} -R 32 'recursive'
-        return 0
+	${CLANGSAY} -b 'borg'				&& \
+		${CLANGSAY} -d 'dead'			&& \
+		${CLANGSAY} -g 'greedy'			&& \
+		${CLANGSAY} -p 'paranoid'		&& \
+		${CLANGSAY} -s 'stoned'			&& \
+		${CLANGSAY} -t 'tired'			&& \
+		${CLANGSAY} -w 'wired'			&& \
+		${CLANGSAY} -y 'youthful'		&& \
+		${CLANGSAY} -e "目" 'eye'		&& \
+		${CLANGSAY} -T "舌" 'tongue'	&& \
+		${CLANGSAY} --think 'think'		&& \
+		${CLANGSAY} --say 'say'			&& \
+		${CLANGSAY} -R 32 'recursive'
+		return 0
 
-    return 1
+	return 1
 }
 
 function test2() {
-    ${CLANGSAY} -R NOINT                || \
-        ${CLANGSAY} -f /nofile          || \
-        ${CLANGSAY} -f /                || \
-        ${CLANGSAY} -f /var/log/dmesg   || \
-        return 0
+	touch ../testsuite/pdfile
+	chmod a-r ../testsuite/pdfile
 
-    return 1
+	${CLANGSAY} -R NOINT					|| \
+		${CLANGSAY} -f /nofile				|| \
+		${CLANGSAY} -f /					|| \
+		${CLANGSAY} -f ../testsuite/pdfile  || \
+		return 0
+
+	return 1
 }
 
 #(test0 && test1 && test2) > /dev/null 2>&1
