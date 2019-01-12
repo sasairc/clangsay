4f5fca0a0dd0b5601c13e67fe07b533478abae74
sasairc <sasairc@ssiserver.moe.hm>
2019-01-12 17:36:28 +0900

    clangsay 1.7.4 (x86_64-linux-gnu)

--- src/config.h
+++ src/config.h
@@ -20,7 +20,7 @@ extern "C" {
 #define PROGNAME        "clangsay"
 #define VERSION         1
 #define PATCHLEVEL      7
-#define SUBLEVEL        3
+#define SUBLEVEL        4
 #define EXTRAVERSION    ""
 #define AUTHOR          "sasairc"
 #define MAIL_TO         "sasairc@ssiserver.moe.hm"
